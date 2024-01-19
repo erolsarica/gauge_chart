d271922c5926fcdb8321e7250c66918fe2ef4826
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2024-01-19 22:29:58 +0300

    radial gauge added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,7 +18,51 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: const Center(child: Text('Hello, world!')),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(40),
+          child: SfRadialGauge(
+            axes: <RadialAxis>[
+              RadialAxis(
+                
+                maximum: 40,
+                interval: 2,
+                labelsPosition: ElementsPosition.outside,
+                ranges: <GaugeRange>[
+                  GaugeRange(
+                    startValue: 0,
+                    endValue: 10,
+                    color: Colors.green,
+                    startWidth: 10,
+                    endWidth: 10,
+                  ),
+                  GaugeRange(
+                    startValue: 10,
+                    endValue: 20,
+                    color: Colors.yellow,
+                    startWidth: 10,
+                    endWidth: 10,
+                  ),
+                  GaugeRange(
+                    startValue: 20,
+                    endValue: 30,
+                    color: Colors.orange,
+                    startWidth: 10,
+                    endWidth: 10,
+                  ),
+                  GaugeRange(
+                    startValue: 30,
+                    endValue: 40,
+                    color: Colors.red,
+                    startWidth: 10,
+                    endWidth: 10,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ),
+      ),
     );
   }
 }
